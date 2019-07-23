8e354fc694ec8f86de169c671588c114db255373
Luis Giraldo <luis.giraldo10@udea.edu.co>
2019-07-23 01:17:10 -0500

    Feat: Cors for api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,33 @@
-import http from 'http';
 import express from 'express';
+import http from 'http';
 import SocketIO from 'socket.io';
 
-import { getMovies } from './services/movieService';
-import { getReservations } from './services/reservation';
+import IMovie from './models/movie';
 import { getMovieFunctions } from './services/movieFunctionService';
+import { getMovies, postMovies } from './services/movieService';
+import { getReservations } from './services/reservation';
 
 const app = express();
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Accept, Accept-Language, Content-Language, Content-Type, Authorization, Content-Length, X-Requested-With, Access-Control-Allow-Origin'
+  );
+  res.header('Content-Type', 'application/json');
+
+  //intercepts OPTIONS method
+  if ('OPTIONS' === req.method) {
+    //respond with 200
+    res.status(200).send('OK');
+  } else {
+    //move on
+    next();
+  }
+});
+
 const server = http.createServer(app);
 
 app.get('/movies', async (req, res) => {
